e423fc50efd8d55be7117bf17dc61b91f5367724
masterhamzaa <master.hamza4576@gmail.com>
2022-09-13 11:36:57 +0200

    commit hamza

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://10.1.0.76:3000"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <center><h1><em>R  E  A  C  T</em></h1></center>
+        
+      
     </div>
   );
 }
