a1baab9f9cb51a0dc53b225465ab232de48595ae
masterhamzaa <master.hamza4576@gmail.com>
2022-09-13 13:40:58 +0200

    h a m z a  c o m m i t

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let p1 = "React a été conçu dès le départ pour une adoption progressive, et vous pouvez utiliser React a minima ou autant que nécessaire. Que vous souhaitiez avoir un aperçu de React, ajouter de l’interactivité à une simple page HTML ou démarrer une application React complète, les liens de cette section vous aideront à bien démarrer.";
+  let p2 = "React a été conçu dès le départ pour une adoption progressive, et vous pouvez utiliser React a minima ou autant que nécessaire. Que vous souhaitiez avoir un aperçu de React, ajouter de l’interactivité à une simple page HTML ou démarrer une application React complète, les liens de cette section vous aideront à bien démarrer.";
+  let p3 = "React a été conçu dès le départ pour une adoption progressive, et vous pouvez utiliser React a minima ou autant que nécessaire. Que vous souhaitiez avoir un aperçu de React, ajouter de l’interactivité à une simple page HTML ou démarrer une application React complète, les liens de cette section vous aideront à bien démarrer.";
+  let h2 = "React pour les débutantes";
+  let p4 = "Si vous trouvez que la documentation React va un peu trop vite pour vous, consultez cet aperçu de React par Tania Rascia. Il présente les concepts les plus importants de React de façon détaillée et conviviale pour les débutant·e·s. Une fois que vous avez terminé, essayez à nouveau la documentation !";
+
+
+
   return (
     <div>
-        <center><h1><em>R  E  A  C  T</em></h1></center>
-        
-      
-    </div>
+      <center><h1><em>R  E  A  C  T   H   A   M   Z  A</em></h1></center>
+      <article>
+  <h1>R e a c t</h1>
+  <p>{p1}</p>
+  <p>{p2}</p>
+
+  <p>{p3}</p>
+  <h2>{h2}</h2>
+  <p>{p4}</p>
+  
+</article>
+    
+  
+</div>
   );
 }
 
